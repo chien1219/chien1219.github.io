91e6a78ad4f600e1e855e8d682efae2e25a46928
Richard Chien <richard85014@hotmail.com>
2018-12-10 23:51:48 +0800

    Add photo and videos

--- assets/js/myfancybox.js
+++ assets/js/myfancybox.js
@@ -32,7 +32,41 @@ $(document).ready(function() {
 		'overlayColor'		: '#000',
 		'overlayOpacity'	: 0.8
 	});
+
 	$("a[rel=DNA]").fancybox({
+		'transitionIn'		: 'elastic',
+		'transitionOut'		: 'elastic',
+		'padding'			: '0',
+		'width'				: '75%',
+		'height'			: '75%',
+		'titlePosition'		: 'outside',
+		'overlayColor'		: '#000',
+		'overlayOpacity'	: 0.8
+	});
+
+	$("a[rel=TM1]").fancybox({
+		'transitionIn'		: 'elastic',
+		'transitionOut'		: 'elastic',
+		'padding'			: '0',
+		'width'				: '75%',
+		'height'			: '75%',
+		'titlePosition'		: 'outside',
+		'overlayColor'		: '#000',
+		'overlayOpacity'	: 0.8
+	});
+
+	$("a[rel=TM2]").fancybox({
+		'transitionIn'		: 'elastic',
+		'transitionOut'		: 'elastic',
+		'padding'			: '0',
+		'width'				: '75%',
+		'height'			: '75%',
+		'titlePosition'		: 'outside',
+		'overlayColor'		: '#000',
+		'overlayOpacity'	: 0.8
+	});
+
+	$("a[rel=PR&F]").fancybox({
 		'transitionIn'		: 'elastic',
 		'transitionOut'		: 'elastic',
 		'padding'			: '0',
