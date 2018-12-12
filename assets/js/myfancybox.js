acbada2489c43e6aabb63ca5d7453732ca3755b3
chien1219 <richard85014@hotmail.com>
2018-12-12 10:50:10 +0800

    primitively finished

--- assets/js/myfancybox.js
+++ assets/js/myfancybox.js
@@ -1,91 +1,12 @@
 $(document).ready(function() {
 	
-	$("a[rel=FS]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8,
+	$('[data-fancybox]').fancybox({
+		 protect: true,
+ 		 transitionEffect: 'tube',
+ 		 buttons : [
+  		  'share',
+  		  'fullScreen',
+  		  'close'
+ 		 ]
 	});
-
-	$("a[rel=EERK4]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8
-	});
-
-	$("a[rel=360]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8
-	});
-
-	$("a[rel=HS]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8
-	});
-
-	$("a[rel=DNA]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8
-	});
-
-	$("a[rel=TM1]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8
-	});
-
-	$("a[rel=TM2]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8
-	});
-
-	$("a[rel=PR&F]").fancybox({
-		'transitionIn'		: 'elastic',
-		'transitionOut'		: 'elastic',
-		'padding'			: '0',
-		'width'				: '75%',
-		'height'			: '75%',
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.8
-	});
-
 });
