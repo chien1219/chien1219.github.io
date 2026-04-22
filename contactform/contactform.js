964415870d0d087049deb9b8ecf909b2d37e1a5c
Richard Chien <lchien@apple.com>
2026-04-22 12:44:10 -0700

    remove redundant ref

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -90,37 +90,9 @@ jQuery(document).ready(function($) {
     });
     if (ferror) return false;
     else var str = $(this).serialize();
-    var action = $(this).attr('action');
-    if( ! action ) {
-      action = 'http://104.32.144.237/contactme/contactme.php';
-    }
-    /*
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html("Sorry, Message server is currently down! Try <a style=\"color: blue\" href = \"mailto: richard85014@gmail.com?subject=" + $subject + "&body=" + $message + "\">email me!</a>");
-        }
-      },
-      error: function(msg){
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html("Sorry, Message server is currently down!");
-      }
-    });
-    */
     var cat = "Name: " + jQuery('input[name="name"]').val() + "<br>"
     + "Email: " + jQuery('input[name="email"]').val() + "<br>"
     + "Message: " + jQuery('textarea[name="message"]').val();
-    alert(cat);
     Email.send({
       SecureToken : "7c3d5fc7-9a03-46d4-a33d-eb5e14f39b01",
       To : 'richard85014@gmail.com',
